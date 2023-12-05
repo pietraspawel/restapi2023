8da1d46fa58242f252a1258a1148497c4177ac53
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-05 14:45:47 +0100

    remove Twig

--- app/src/Application.php
+++ app/src/Application.php
@@ -13,18 +13,12 @@ class Application
     private $controller;
     private $database;
     private $mode;
-    /**
-     * @var \Twig\Environment $twig Twig object.
-     */
-    private $twig;
-    private $user;
 
     public function __construct(string $configFilepath = "../config/application.yaml")
     {
         $this->config = Yaml::parseFile($configFilepath);
         $this->__initVariables();
         $this->__setErrorReporting($this->mode);
-        $this->__initTwig();
         $this->__initDatabase();
     }
 
@@ -41,18 +35,6 @@ class Application
         } else {
             error_reporting(0);
         }
-    }
-
-    protected function __initTwig()
-    {
-        $debug = $this->mode == "dev" ? true : false;
-        $loader = new \Twig\Loader\FilesystemLoader($this->config["templates_path"]);
-        $this->twig = new \Twig\Environment($loader, [
-            "cache" => $this->config["cache_path"],
-            "debug" => $debug,
-            "strict_variables" => true,
-        ]);
-        $this->twig->addExtension(new \Twig\Extension\DebugExtension());
     }
 
     protected function __initDatabase()
