ce5c902d51b5cf2c39fbf33f0f59fc9e86442c80
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-05 14:29:33 +0100

    ch RestMethods

--- app/src/RestMethods.php
+++ app/src/RestMethods.php
@@ -7,7 +7,7 @@ namespace pietras\RestApi;
  */
 class RestMethods
 {
-    public function send200AndJson($json)
+    public static function send200AndJson($json)
     {
         header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
         header("Cache-Control: public, max-age=86400");
@@ -15,25 +15,25 @@ class RestMethods
         echo json_encode($json);
     }
 
-    public function send200()
+    public static function send200()
     {
         header($_SERVER["SERVER_PROTOCOL"] . " 200 OK.");
         header("Cache-Control: no-cache, no-store, must-revalidate");
     }
 
-    public function send201()
+    public static function send201()
     {
         header($_SERVER["SERVER_PROTOCOL"] . " 201 OK.");
         header("Cache-Control: no-cache, no-store, must-revalidate");
     }
 
-    public function send400()
+    public static function send400()
     {
         header($_SERVER["SERVER_PROTOCOL"] . " 400 Data error.");
         header("Cache-Control: no-cache, no-store, must-revalidate");
     }
 
-    public function send404()
+    public static function send404()
     {
         header($_SERVER["SERVER_PROTOCOL"] . " 404 Source not found.");
         header("Cache-Control: no-cache, no-store, must-revalidate");
