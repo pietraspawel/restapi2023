3a1703f9d94b99a9ff6f219a2e981807e00f2dda
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-06 11:00:43 +0100

    add ProductModel::fetchAll()

--- app/controller/test.php
+++ app/controller/test.php
@@ -1,40 +1,11 @@
 <?php
 
+use pietras\RestApi\ProductModel;
 use pietras\RestApi\UserMethods;
 use pietras\RestApi\UserModel;
 
 echo "<pre>";
 
 //////
-$username = "testuser";
-
-$user = UserModel::fetchUserByName($application, $username);
-var_dump($user);
-$user = UserModel::fetchUserByName($application, "");
-var_dump($user);
-$user = UserModel::fetchUserByName($application, "wrong");
-var_dump($user);
-
-$user = UserModel::isReadingPermitted($application, $username, "kupa");
-var_dump($user);
-
-$user = UserModel::isReadingPermitted($application, $username, "123");
-var_dump($user);
-
-$user = UserModel::isReadingPermitted($application, "wrong", "123");
-var_dump($user);
-
-$user = UserModel::isReadingPermitted($application, "reader", "123");
-var_dump($user);
-
-$user = UserModel::isWritingPermitted($application, $username, "kupa");
-var_dump($user);
-
-$user = UserModel::isWritingPermitted($application, $username, "123");
-var_dump($user);
-
-$user = UserModel::isWritingPermitted($application, "wrong", "123");
-var_dump($user);
-
-$user = UserModel::isWritingPermitted($application, "reader", "123");
-var_dump($user);
+$result = ProductModel::fetchAll($application, 3, 10);
+var_dump($result);
--- app/src/ProductModel.php
+++ app/src/ProductModel.php
@@ -11,4 +11,49 @@ class ProductModel
     private $price;
     private $quantity;
     private $translation;
+
+    public function __construct(array $param)
+    {
+        $this->id = $param["id"];
+        $this->price = $param["price"];
+        $this->quantity = $param["quantity"];
+        $this->translation = $param["translation"];
+    }
+
+    public static function fetchAll(Application $application, int $page = 1, int $pagesize = 10): array
+    {
+        $database = $application->getDatabase();
+        $offset = ($page - 1) * $pagesize;
+        $sql = "
+            SELECT 
+                p.*, 
+                product_name.name AS name,
+                product_name.description AS description,
+                language.abbr AS language
+            FROM ( SELECT * FROM product LIMIT $offset, $pagesize) AS p
+            INNER JOIN product_name ON p.id = product_name.product_id
+            INNER JOIN language ON product_name.language_id = language.id
+        ";
+        $stmt = $database->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return self::resultToCollection($result);
+    }
+
+    private static function resultToCollection(array $array): array
+    {
+        $coll = [];
+        $objColl = [];
+        foreach ($array as $row) {
+            $coll[$row["id"]]["id"] = $row["id"];
+            $coll[$row["id"]]["price"] = $row["price"];
+            $coll[$row["id"]]["quantity"] = $row["quantity"];
+            $coll[$row["id"]]["translation"][$row["language"]]["name"] = $row["name"];
+            $coll[$row["id"]]["translation"][$row["language"]]["description"] = $row["description"];
+        }
+        foreach ($coll as $key => $obj) {
+            $objColl[$key] = new self($obj);
+        }
+        return $objColl;
+    }
 }
