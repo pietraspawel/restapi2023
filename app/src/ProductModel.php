4c2cb1b327cc7c71fb4b02144bc79dd39d6eb0f8
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-06 09:55:31 +0100

    add ProductModel

---
+++ app/src/ProductModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pietras\RestApi;
+
+/**
+ * Provide product model and its methods.
+ */
+class ProductModel
+{
+    private $id;
+    private $price;
+    private $quantity;
+    private $translation;
+}
